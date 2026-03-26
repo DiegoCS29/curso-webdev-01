2076d09313334a3a42fde1f43694dbeadd453ba2
Diego <diego.candido.stoianof@gmail.com>
2026-03-25 22:22:56 -0300

    Aula02

---
+++ Aula2/script.js
@@ -0,0 +1,50 @@
+// //Operadores básicos
+// let a = 5, b = 10;
+
+// console.log(a == b);   // false (igualdade)
+// console.log(a === b);  // false (igualdade estrita)
+// console.log(a != b);   // true (diferente)
+// console.log(a !== b);  // true (diferente estrito)
+// console.log(a < b);    // true (menor que)
+// console.log(a > b);    // false (maior que)
+// console.log(a <= b);   // true (menor ou igual)
+// console.log(a >= b);   // false (maior ou igual)
+
+// let idade = 25;
+// let temCarteira = false;
+
+// // AND (&&) - ambas condições devem ser verdadeiras
+// if (idade >= 18 && temCarteira) {
+//   console.log("Pode dirigir");
+// }
+
+// OR (||) - pelo menos uma condição deve ser verdadeira
+// if (idade < 18 || !temCarteira) {
+//   console.log("Não pode dirigir");
+// }
+
+// NOT (!) - inverte o valor booleano
+// if (!temCarteira) {
+//   console.log("Precisa de carteira");
+// }
+
+//Switch Case
+// let diaSemana = new Date().getDay(); // 0: Domingo, 1: Segunda, etc.
+
+// switch (diaSemana) {
+//   case 0:
+//     console.log("Domingo");
+//     break;
+//   case 1:
+//     console.log("Segunda-feira");
+//     break;
+//   // ...
+//   default:
+//     console.log("Dia inválido");
+//     break;
+// }
+
+// //FOR
+// for (let i = 0; i < 5; i++) {
+//   console.log("Contagem:", i);
+// }
